d8eceb6cd0b5c0a9566ee53c49e9f5a944e0085d
ulasgokcan-nsn <ulasgokcan@gmail.com>
2025-11-28 14:48:41 +0300

    Add files via upload
    first file is empty idk why but this one is okey

---
+++ 100.yas.hesaplayıcı.py
@@ -0,0 +1,11 @@
+isim = input('İsminiz Nedir: ')
+yas = int(input('Kaç Yaşındasın: '))
+mevcuttarih = 2025
+kacsenekaldı = 100 - yas
+hangiyil = mevcuttarih + kacsenekaldı
+
+a = str(isim)
+b = str(kacsenekaldı)
+c = str(hangiyil)
+
+print('Sevgili ' + a + ',' + ' ' + b + ' sene sonra 100 yaşında olacaksın, ' + ' ' + 'Yani ' + c + ' yılında!')
